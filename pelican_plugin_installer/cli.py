d786fe90ab727f90f027dc77ca0596db035bcc76
Klaus Laube <kplaube@gmail.com>
2016-11-12 13:27:02 -0200

    Change default tests to cover the new interface

--- pelican_plugin_installer/cli.py
+++ pelican_plugin_installer/cli.py
@@ -2,10 +2,10 @@ import click
 
 
 @click.command()
-@click.option('--delete', 'operation', flag_value='delete', help='Delete a specific plugin.')
-@click.option('--install', 'operation', flag_value='install', help='Install a specific plugin.')
-@click.option('--update', 'operation', flag_value='update', help='Update a specific plugin.')
-@click.argument('plugin_name', default='world', required=True)
+@click.option('--delete', '-d', 'operation', flag_value='delete', help='Delete a specific plugin.')
+@click.option('--install', '-i', 'operation', flag_value='install', help='Install a specific plugin.')
+@click.option('--update', '-u', 'operation', flag_value='update', help='Update a specific plugin.')
+@click.argument('plugin_name', required=False)
 def main(plugin_name, operation):
     """Installs Pelican Plugins in an easy way"""
     click.echo("Operation: {0}; Plugin: {1}".format(operation, plugin_name))
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,18 +12,25 @@ def test_cli(runner):
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
     assert not result.exception
-    assert result.output.strip() == 'Hello, world.'
+    assert result.output.strip() == 'Operation: None; Plugin: None'
 
 
-def test_cli_with_option(runner):
-    result = runner.invoke(cli.main, ['--as-cowboy'])
-    assert not result.exception
-    assert result.exit_code == 0
-    assert result.output.strip() == 'Howdy, world.'
-
-
-def test_cli_with_arg(runner):
-    result = runner.invoke(cli.main, ['Klaus'])
+def test_installing_a_plugin(runner):
+    result = runner.invoke(cli.main, ['-i', 'test'])
     assert result.exit_code == 0
     assert not result.exception
-    assert result.output.strip() == 'Hello, Klaus.'
+    assert result.output.strip() == 'Operation: install; Plugin: test'
+
+
+def test_updating_a_plugin(runner):
+    result = runner.invoke(cli.main, ['-u', 'test'])
+    assert result.exit_code == 0
+    assert not result.exception
+    assert result.output.strip() == 'Operation: update; Plugin: test'
+
+
+def test_removing_a_plugin(runner):
+    result = runner.invoke(cli.main, ['-d', 'test'])
+    assert result.exit_code == 0
+    assert not result.exception
+    assert result.output.strip() == 'Operation: delete; Plugin: test'
