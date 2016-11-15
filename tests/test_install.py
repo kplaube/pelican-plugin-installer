f64cf7aaf72baaf52b4fab5ed7c3b8203b408c49
Klaus Laube <kplaube@gmail.com>
2016-11-15 20:32:37 -0200

    Add more tests

--- tests/test_install.py
+++ tests/test_install.py
@@ -3,28 +3,37 @@ from pelican_plugin_installer import (
     GIT_CLONE_COMMAND, GIT_SUBMODULE_COMMAND, PLUGINS_LOCAL_REPOSITORY, os, shutil)
 
 
-class TestInstallAPlugin:
+def test_discover_a_pelican_conf_file(pelicanconf, runner):
+    runner.invoke(cli.main, ['-i', 'plugin-name', '-c', 'pelicanconf.py'])
 
-    def test_discover_a_pelican_conf_file(self, pelicanconf, runner):
-        runner.invoke(cli.main, ['-i', 'plugin-name', '-c', 'pelicanconf.py'])
+    assert pelicanconf.call_count == 1
 
-        assert pelicanconf.call_count == 1
 
-    def test_initialize_local_repository(self, mocker, pelicanconf, mock_file_operations, runner):
-        runner.invoke(cli.main, ['-i', 'plugin-name', '-c', 'pelicanconf.py'])
+def test_initialize_local_repository(mocker, mock_file_operations, runner):
+    runner.invoke(cli.main, ['-i', 'plugin-name', '-c', 'pelicanconf.py'])
 
-        expected = (
-            mocker.call(GIT_CLONE_COMMAND, ),
-            mocker.call(GIT_SUBMODULE_COMMAND, ),
-        )
+    expected = (
+        mocker.call(GIT_CLONE_COMMAND, ),
+        mocker.call(GIT_SUBMODULE_COMMAND, ),
+    )
 
-        os.makedirs.assert_called_once_with(PLUGINS_LOCAL_REPOSITORY)
-        os.system.assert_has_calls(expected)
+    os.makedirs.assert_called_once_with(PLUGINS_LOCAL_REPOSITORY)
+    os.system.assert_has_calls(expected)
 
-    def test_install_plugin_by_copying_its_files_to_plugins_path(self, pelicanconf, mock_file_operations, runner):
-        runner.invoke(cli.main, ['-i', 'plugin-name', '-c', 'pelicanconf.py'])
 
-        src_path = os.path.join(PLUGINS_LOCAL_REPOSITORY, 'plugin-name')
-        dst_path = '/tmp/plugins/plugin-name'
+def test_install_plugin_by_copying_its_files_to_plugins_path(mock_file_operations, runner):
+    runner.invoke(cli.main, ['-i', 'plugin-name', '-c', 'pelicanconf.py'])
 
-        shutil.copytree.assert_called_once_with(src_path, dst_path)
+    src_path = os.path.join(PLUGINS_LOCAL_REPOSITORY, 'plugin-name')
+    dst_path = '/tmp/plugins/plugin-name'
+
+    shutil.copytree.assert_called_once_with(src_path, dst_path)
+
+
+def test_warn_when_the_plugin_is_already_installed(mocker, runner):
+    os_path_exists = mocker.patch('os.path.exists')
+    os_path_exists.return_value = True
+
+    result = runner.invoke(cli.main, ['-i', 'plugin-name', '-c', 'pelicanconf.py'])
+
+    assert 'The plugin is already installed' in result.output.strip()
