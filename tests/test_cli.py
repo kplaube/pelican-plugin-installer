938e28f54de9ed91fd0f9e7208df5f432b37f1fe
Klaus Laube <kplaube@gmail.com>
2016-11-15 19:58:21 -0200

    Ensure copying plugin files to PLUGINS_PATH is executing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,7 @@ import pytest
 import sys
 from click.testing import CliRunner
 from pelican_plugin_installer import cli
-from pelican_plugin_installer.cli import os
+from pelican_plugin_installer.cli import os, shutil
 
 
 @pytest.fixture
@@ -29,16 +29,18 @@ def mock_file_operations(mocker):
     mocker.patch('os.makedirs')
     mocker.patch('os.system')
 
+    mocker.patch('shutil.copytree')
+
 
 class TestInstallAPlugin:
 
     def test_discover_a_pelican_conf_file(self, pelicanconf, runner):
-        runner.invoke(cli.main, ['-i', 'test', '-c', 'pelicanconf.py'])
+        runner.invoke(cli.main, ['-i', 'plugin-name', '-c', 'pelicanconf.py'])
 
         assert pelicanconf.call_count == 1
 
     def test_initialize_local_repository(self, mocker, pelicanconf, mock_file_operations, runner):
-        runner.invoke(cli.main, ['-i', 'test', '-c', 'pelicanconf.py'])
+        runner.invoke(cli.main, ['-i', 'plugin-name', '-c', 'pelicanconf.py'])
 
         expected = (
             mocker.call(cli.GIT_CLONE_COMMAND, ),
@@ -47,3 +49,11 @@ class TestInstallAPlugin:
 
         os.makedirs.assert_called_once_with(cli.PLUGINS_LOCAL_REPOSITORY)
         os.system.assert_has_calls(expected)
+
+    def test_install_plugin_by_copying_its_files_to_plugins_path(self, pelicanconf, mock_file_operations, runner):
+        runner.invoke(cli.main, ['-i', 'plugin-name', '-c', 'pelicanconf.py'])
+
+        src_path = os.path.join(cli.PLUGINS_LOCAL_REPOSITORY, 'plugin-name')
+        dst_path = '/tmp/plugins/plugin-name'
+
+        shutil.copytree.assert_called_once_with(src_path, dst_path)
